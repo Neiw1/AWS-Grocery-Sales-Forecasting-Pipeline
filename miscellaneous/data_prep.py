3482c878537762d45cf610e9b55f7108dd3f4f9b
Neiw1 <wsiraphop@gmail.com>
2025-07-03 19:47:32 +0700

    add miscellaneous folder

---
+++ miscellaneous/data_prep.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv('engineered_test.csv')
+
+data_to_cloud = df[df['date'] < '2017-08-10']
+data_to_test = df[df['date'] > '2017-08-10']
+
+drop_cols = ['sale_lag_1', 'sale_lag_7', 'promo_lag_1', 'promo_lag_7']
+data_to_test = df.drop(columns=drop_cols)
+data_to_test.to_csv("test_for_prediction.csv", index=False)
+data_to_cloud.to_csv("for_uploading.csv", index=False)
